907174833a87c5ad3bf90b5a2499068552032ec7
jhonatan810 <jhonatan8100@gmail.com>
2015-11-29 23:10:10 -0600

    Prueba
    Descripcion prueba

---
+++ Prueba2D/Assets/Scripts/ControladorPersonaje.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControladorPersonaje : MonoBehaviour {
+
+	public float fuerzaSalto = 100f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	} 
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			GetComponent<Rigidbody2D>().AddForce(Vector2.up * fuerzaSalto);
+		}
+
+
+	}
+}
